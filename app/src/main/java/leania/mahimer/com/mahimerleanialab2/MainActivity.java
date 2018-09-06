d195ecb02181c71a9628783de44d5507625b339d
jamesearltan <jamesearl.tan.iics@ust.edu.ph>
2018-09-06 14:15:38 +0800

    Initial commit

---
+++ app/src/main/java/leania/mahimer/com/mahimerleanialab2/MainActivity.java
@@ -0,0 +1,15 @@
+package leania.mahimer.com.mahimerleanialab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.linearlayout);
+        //setContentView(R.layout.relativelayout);
+        setContentView(R.layout.constraintlayout);
+    }
+}
